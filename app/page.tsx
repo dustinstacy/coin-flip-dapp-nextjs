cbe002c83e1f3acadda6af2a4cbc1f78863aaf98
Dustin Stacy <dustin.stacy@outlook.com>
2023-11-12 13:50:09 -0500

    Add connectWallet function

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,52 @@
+'use client'
+
+import { ethers } from 'ethers'
+import { useEffect, useState } from 'react'
+
 export default function Home() {
+    const [isConnected, setIsConnected] = useState(false)
+    const [hasMetamask, setHasMetamask] = useState(false)
+
+    let provider
+    let signer = null
+
+    const connectWallet = async () => {
+        const { ethereum } = window
+
+        if (ethereum !== 'undefined') {
+            try {
+                await ethereum.request({ method: 'eth_requestAccounts' })
+                setIsConnected(true)
+                provider = new ethers.BrowserProvider(ethereum)
+                signer = await provider.getSigner()
+            } catch (e) {
+                console.log(e)
+            }
+        } else {
+            setIsConnected(false)
+        }
+    }
+
+    useEffect(() => {
+        if (typeof window.ethereum !== 'undefined') {
+            setHasMetamask(true)
+        }
+    })
+
     return (
         <main className='flex min-h-screen flex-col items-center justify-between p-24'>
             Coin Flip Dapp
+            <div>
+                {hasMetamask ? (
+                    isConnected ? (
+                        'Connected! '
+                    ) : (
+                        <button onClick={() => connectWallet()}>Connect</button>
+                    )
+                ) : (
+                    'Please install metamask'
+                )}
+            </div>
             <div className='w-1/4 flex justify-between'>
                 <button>Heads</button>
                 <button>Tails</button>
