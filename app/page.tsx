e4c3a3fdb76c9d3801dd45039eb7057ce7cda294
Dustin Stacy <dustin.stacy@outlook.com>
2023-11-17 18:04:22 -0500

    Add fund button and fix wager function

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,9 @@
 import { ethers } from 'ethers'
 import React, { useEffect, useState } from 'react'
 import { abi, contractAddresses } from '@constants'
-import { BrowserProvider, Signer } from 'ethers'
 import { Contract } from 'ethers'
+import { ContractRunner } from 'ethers'
+import { Flow_Rounded } from 'next/font/google'
 
 interface ContractAddresses {
     [chainId: number]: string[]
@@ -13,33 +14,17 @@ interface ContractAddresses {
 export default function Home() {
     const [isConnected, setIsConnected] = useState(false)
     const [hasMetamask, setHasMetamask] = useState(false)
-    const [wager, setWager] = useState(0.1)
-
-    let provider: BrowserProvider
-    let signer: Signer
-    let chainId: bigint
-    let contractAddress: string | undefined
-    let coinFlipContract: Contract | undefined
+    const [wager, setWager] = useState('0.1')
+    const [amount, setAmount] = useState('0.1')
+    const { ethereum } = window
 
     const addressArray: ContractAddresses = contractAddresses
 
-    const connectWallet = async () => {
-        const { ethereum } = window
-
-        if (ethereum !== 'undefined') {
+    const connect = async () => {
+        if (typeof ethereum != 'undefined') {
             try {
-                await ethereum.request({ method: 'eth_requestAccounts' })
                 setIsConnected(true)
-                provider = new ethers.BrowserProvider(ethereum)
-                signer = await provider.getSigner()
-                chainId = (await provider.getNetwork()).chainId
-                contractAddress = addressArray[Number(chainId)]
-                    ? addressArray[Number(chainId)][0]
-                    : undefined
-                coinFlipContract = contractAddress
-                    ? new ethers.Contract(contractAddress, abi, provider)
-                    : undefined
-                console.log(provider, signer, chainId, contractAddress)
+                await ethereum.request({ method: 'eth_requestAccounts' })
             } catch (e) {
                 console.log(e)
             }
@@ -48,13 +33,72 @@ export default function Home() {
         }
     }
 
+    // const connectWallet = async () => {
+    //     const { ethereum } = window
+    //     const accounts = await ethereum.request({ method: 'eth_requestAccounts' })
+    //     setWalletAddress(accounts[0])
+    //     console.log(accounts)
+
+    //     if (ethereum !== 'undefined') {
+    //         try {
+    //             setIsConnected(true)
+    //             provider = new ethers.BrowserProvider(ethereum)
+    //             chainId = (await provider.getNetwork()).chainId
+    //             console.log(chainId)
+    //             contractAddress = addressArray[Number(chainId)]
+    //                 ? addressArray[Number(chainId)][0]
+    //                 : undefined
+    //             console.log(contractAddress)
+    //             setCoinFlipContract(new ethers.Contract(contractAddress!, abi, provider))
+    //         } catch (e) {
+    //             console.log(e)
+    //         }
+    //     } else {
+    //         setIsConnected(false)
+    //     }
+    // }
+
     const handleWagerChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { value } = e.target
-        setWager(Number(value))
+        setWager(value)
     }
 
-    const placeWager = (entrantsGuess: Number, entrantsWager: Number) => {
-        coinFlipContract?.enterWager()
+    const handleFundingChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { value } = e.target
+        setAmount(value)
+    }
+
+    const placeWager = async (entrantsGuess: Number, wager: string) => {
+        if (typeof ethereum !== 'undefined') {
+            const provider = new ethers.BrowserProvider(ethereum)
+            const signer = await provider.getSigner()
+            const chainId = (await provider.getNetwork()).chainId
+            const contract = new ethers.Contract(addressArray[Number(chainId)][0], abi, signer)
+            try {
+                console.log(wager)
+                const tx = await contract.enterWager(entrantsGuess, {
+                    value: ethers.parseEther(wager),
+                })
+            } catch (e) {
+                console.log(e)
+            }
+        }
+    }
+
+    const fund = async (amount: string) => {
+        if (typeof ethereum !== 'undefined') {
+            const provider = new ethers.BrowserProvider(ethereum)
+            const signer = await provider.getSigner()
+            const chainId = (await provider.getNetwork()).chainId
+            const contract = new ethers.Contract(addressArray[Number(chainId)][0], abi, signer)
+            try {
+                const tx = await contract.fund({
+                    value: ethers.parseEther(amount),
+                })
+            } catch (e) {
+                console.log(e)
+            }
+        }
     }
 
     useEffect(() => {
@@ -69,9 +113,17 @@ export default function Home() {
             <div>
                 {hasMetamask ? (
                     isConnected ? (
-                        'Connected! '
+                        <>
+                            <input
+                                className='text-black text-center w-[50px]'
+                                value={amount}
+                                type='string'
+                                onChange={handleFundingChange}
+                            />
+                            <button onClick={() => fund(amount)}>Fund</button>
+                        </>
                     ) : (
-                        <button onClick={() => connectWallet()}>Connect</button>
+                        <button onClick={() => connect()}>Connect</button>
                     )
                 ) : (
                     'Please install metamask'
@@ -79,11 +131,8 @@ export default function Home() {
             </div>
             <input
                 className='text-black text-center w-[50px]'
-                type='number'
-                step={0.1}
-                min={0.1}
-                max={5.0}
-                value={wager.toFixed(1)}
+                type='string'
+                value={wager}
                 onChange={handleWagerChange}
             />
             <div className='w-1/4 flex justify-between'>
